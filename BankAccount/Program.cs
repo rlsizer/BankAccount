592bf4455f00dfe0d949e7bc8fd73ea90423e1ec
rlsizer <theryanlilo@gmail.com>
2017-04-17 12:39:23 -0400

    Added menu greeting.

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -10,10 +10,27 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            //Bank Account Project
+            //Ryan Sizer
+            //Week 3 - 04/17/2017
+
+
             Client client1 = new Client();
 
-            client1.DisplayClientInfo();
+            //client1.DisplayClientInfo();
 
+
+            //Greet the user
+            Console.WriteLine("Hello. Welcome to your Bank Account.\r\n");
+
+            //Store menu options in a string
+            string menu = ("----------------------------------" + Environment.NewLine +
+              "- [1] View Client Information" + Environment.NewLine);
+
+            //Display menu options
+            Console.WriteLine(menu);
+
+            
         }
     }
 }
